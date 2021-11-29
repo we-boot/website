d517eaf79fe393e4a50aeaf5904ebad2654eb2bd
Stijn Rogiest <reddusted@gmail.com>
2021-11-29 11:21:45 +0100

    Create basic front page with gradient background, upload phone models

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import THREE from "three";
 
 export default function Home() {
-    return <div>this is a test</div>;
+    // return <div style={{ backgroundImage: "url(/gradient.svg)", backgroundSize: "cover", minHeight: "100vh", width: "100%" }}></div>;
+    return (
+        <div>
+            <div
+                style={{
+                    height: "100vh",
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative",
+                    background: "url(/gradient.png)",
+                    backgroundSize: "100vw 101vh",
+                    color: "white",
+                }}>
+                <nav className="p-6">
+                    <h1 className="font-bold text-2xl leading-3">weboot</h1>
+                    <p className="opacity-50 text-lg">we solve digital challenges</p>
+                </nav>
+                <div className="flex flex-grow items-center">
+                    <div className="flex-grow">canvas here</div>
+                    <div className="flex-grow">text here</div>
+                </div>
+            </div>
+            {/* <div>next page here</div> */}
+        </div>
+    );
 }
