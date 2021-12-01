162257adf535714d2555eead3f9f041c7e3b600f
Stijn Rogiest <reddusted@gmail.com>
2021-12-01 13:22:22 +0100

    Quality grid clock & icons

--- components/QualityGrid.tsx
+++ components/QualityGrid.tsx
@@ -21,6 +21,8 @@ import {
     AWSIcon,
     LinuxIcon,
 } from "./TechnologyIcons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faHourglass, faSign, faSignal } from "@fortawesome/free-solid-svg-icons";
 
 function QualityCard(props: React.HTMLAttributes<HTMLDivElement> & { title: React.ReactNode; description: React.ReactNode }) {
     return (
@@ -65,6 +67,7 @@ function TechnologyIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
 export function QualityGrid() {
     const techonologiesRef = useRef<HTMLDivElement>(null);
     const technologyTween = useRef<gsap.core.Tween>();
+    const clockRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         technologyTween.current = gsap
@@ -74,8 +77,11 @@ export function QualityGrid() {
                 { x: -56 * (TECH_ICONS.length + 1), duration: TECH_ICONS.length + 1, ease: Linear.easeNone }
             )
             .repeat(-1);
+
+        let clockTween = gsap.to(clockRef.current!, { rotate: 360, duration: 16, ease: Linear.easeNone }).repeat(-1);
         return () => {
             technologyTween.current!.kill();
+            clockTween.kill();
         };
     }, []);
 
@@ -83,34 +89,55 @@ export function QualityGrid() {
         <div style={{ gridTemplateColumns: "repeat(2, 300px)", gridTemplateRows: "repeat(5, 300px)" }} className="grid gap-10 ">
             <QualityCard
                 description="We are productivity freaks. Software is built in one take in a matter of weeks, not months."
-                title="Development time"></QualityCard>
-            <QualityCard description="Every application consists of a modern looking user interface." title="Modern"></QualityCard>
-            <QualityCard description="We build software using the following tech" title="We speak">
-                <div
-                    className="relative mt-4"
-                    onMouseEnter={() => technologyTween.current!.pause()}
-                    onMouseLeave={() => technologyTween.current!.resume()}>
-                    <div ref={techonologiesRef} className="absolute top-0 left-0 min-w-max">
-                        {TECH_ICONS.map((e, i) => (
-                            <TechnologyIcon title={e.name}>{e.icon}</TechnologyIcon>
-                        ))}
-                        {TECH_ICONS.map((e, i) => (
-                            <TechnologyIcon title={e.name}>{e.icon}</TechnologyIcon>
-                        ))}
+                title="Development time">
+                <div className="flex-grow flex justify-center mt-4">
+                    <div className="bg-white bg-opacity-10 w-32 h-32 rounded-full relative">
+                        <div
+                            className=" bg-white bg-opacity-10 h-4 w-16 rounded-lg transform absolute"
+                            style={{ top: 56, left: 56, transformOrigin: "8px 8px" }}
+                            ref={clockRef}></div>
                     </div>
                 </div>
             </QualityCard>
-            <QualityCard description="Our software runs on any device of any size, as long as it supports a web browser." title="Responsive">
-                <div className="flex justify-center flex-grow">
+            {/* <QualityCard description="Every application consists of a modern looking user interface." title="Modern"></QualityCard> */}
+            <QualityCard description="Our software updates automatically, the user won't even notice." title="Updates">
+                <div className="flex-grow flex justify-center mt-4 text-white text-opacity-10">
+                    <FontAwesomeIcon size={"8x"} icon={faArrowDown} />
+                </div>
+            </QualityCard>
+            <QualityCard
+                description="Our software has the possibility to run on any device of any size, as long as it supports a web browser."
+                title="Responsive">
+                <div className="flex justify-center flex-grow mt-4">
                     <div className="bg-white bg-opacity-10 rounded-lg w-28 relative">
-                        <div className="bg-white bg-opacity-10 rounded-lg w-8 h-12 absolute bottom-0 left-0"></div>
+                        <div className="bg-white bg-opacity-10 rounded-lg w-16 h-20 absolute bottom-0 left-0"></div>
                     </div>
                 </div>
             </QualityCard>
-            <QualityCard description="Our software updates automatically, the user won't even notice." title="Updates"></QualityCard>
             <QualityCard
                 description="Because our software runs in the cloud, it has the possibility to be reached from any location."
-                title="Reachable"></QualityCard>
+                title="Reachable">
+                <div className="flex-grow flex justify-center mt-4 text-white text-opacity-10">
+                    <FontAwesomeIcon size={"8x"} icon={faSignal} />
+                </div>
+            </QualityCard>
+            <QualityCard description="We build software using the following tech" title="We speak">
+                <div className="flex-grow flex flex-col justify-center">
+                    <div
+                        className="relative mb-10"
+                        onMouseEnter={() => technologyTween.current!.pause()}
+                        onMouseLeave={() => technologyTween.current!.resume()}>
+                        <div ref={techonologiesRef} className="absolute top-0 left-0 min-w-max">
+                            {TECH_ICONS.map((e, i) => (
+                                <TechnologyIcon title={e.name}>{e.icon}</TechnologyIcon>
+                            ))}
+                            {TECH_ICONS.map((e, i) => (
+                                <TechnologyIcon title={e.name}>{e.icon}</TechnologyIcon>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </QualityCard>
         </div>
     );
 }
